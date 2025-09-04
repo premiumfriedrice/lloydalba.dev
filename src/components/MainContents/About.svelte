7c1d53fffeb5ef076b27cb4e525867f0e7554ea3
premiumfriedrice <lloyddalba@gmail.com>
2025-09-04 10:37:57 -0500

    Updated description

--- src/components/MainContents/About.svelte
+++ src/components/MainContents/About.svelte
@@ -3,23 +3,14 @@
         <h2 class="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">About</h2>
     </div>
     <p class="mb-4">
-      Class of 2027 
-      <a class="font-medium text-slate-100 hover:text-cyan-300 focus-visible:text-cyan-300" href="https://catalog.tamu.edu/undergraduate/engineering/industrial-systems/data-engineering-bs/" target="_blank" rel="noreferrer noopener">B.Sc. in Data Engineering</a> &
-      <a class="font-medium text-slate-100 hover:text-cyan-300 focus-visible:text-cyan-300" href="https://catalog.tamu.edu/undergraduate/arts-and-sciences/mathematics/applied-mathematics-bs-computational-science-emphasis/" target="_blank" rel="noreferrer noopener">B.Sc. Applied Mathematics</a>
+      Class of 2028
+      <a class="font-medium text-slate-100 hover:text-cyan-300 focus-visible:text-cyan-300" href="https://catalog.tamu.edu/undergraduate/engineering/industrial-systems/data-engineering-bs/" target="_blank" rel="noreferrer noopener">B.Sc. in Data Engineering</a>
       at 
       <a class="font-medium text-slate-100 hover:text-cyan-300 focus-visible:text-cyan-300" href="https://www.tamu.edu/index.html" target="_blank" rel="noreferrer noopener">Texas A&M University</a>.
     </p>
 
     <p class="mb-4">
       I have interned 3x as a Software Engineer in platform engineering at a <a class="font-medium text-slate-100 hover:text-cyan-300 focus-visible:text-cyan-300" href="https://www.arrivelogistics.com/" target="_blank" rel="noreferrer noopener">major logistics company</a>.    </p>
-
-    <p class="mb-4">
-      I am also a Game Engine Developer at Anvil Studio. 
-    </p>
-    
-    <p class="mb-4">
-      In my free time, I enjoy learning Graphics Programming and designing performant code, aiming to create math visualizations using C++ and shaders.
-    </p>
 
     <p class="mb-4">
       When I'm not at the computer I'm usually training MMA, enjoying mediterranean food, or spending time with family. 
